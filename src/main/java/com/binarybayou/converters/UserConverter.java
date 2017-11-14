0121ee014db03421e506be8a159a3541310b088d
sev <jleaton@uno.edu>
2017-11-13 19:28:06 -0600

    Adds implementation for user converter

--- src/main/java/com/binarybayou/converters/UserConverter.java
+++ src/main/java/com/binarybayou/converters/UserConverter.java
@@ -13,13 +13,23 @@ public class UserConverter {
 
     public UserView convertToView(User domain) {
         UserView view = new UserView(); //this will be the newly created View version of the domain Object
+        view.setId(domain.getId());
+        view.setFirstName(domain.getFirstName());
+        view.setLastName(domain.getLastName());
+        view.setEmail(domain.getEmail());
+        view.setPhoneNumber(domain.getPhoneNumber());
 
         return view;    //return the View version of the given domain Object
     }
 
     public User convertToDomain(UserView view) throws ValidationException {
         User domain = new User();   //this will be the newly created Domain version of the view Object
-
+        domain.setId(view.getId());
+        domain.setFirstName(view.getFirstName());
+        domain.setLastName(view.getLastName());
+        domain.setEmail(view.getEmail());
+        domain.setPhoneNumber(view.getPhoneNumber());
+        
         return domain;
     }
 }
